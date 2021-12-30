f8eda6f78dfbb96bb1884b83d2e67786ddf7c6f6
fmarvelsk <fmarvelsk@gmail.com>
2021-12-30 16:16:52 +0100

    bread crumbs

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -7,8 +7,14 @@ import SearchTopBar from "@components/shop/top-bar";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { GetStaticProps } from "next";
 import { useGQLQuery } from "@framework/product/product-queries";
+import StickyBox from "react-sticky-box";
+import ActiveLink from "@components/ui/active-link";
+import { BreadcrumbItems } from "@components/common/breadcrumb";
+import { useTranslation } from "next-i18next";
 
 export default function Products() {
+  const { t } = useTranslation("common");
+  
   const {
     data,
     isFetching: isLoading,
@@ -21,7 +27,28 @@ export default function Products() {
     <>
       <Container>
         <div className={`flex pt-8 pb-16 lg:pb-20`}>
-       
+        <div className="flex-shrink-0 pe-24 hidden lg:block w-96">
+            <StickyBox offsetTop={50} offsetBottom={20}>
+              <div className="pb-7">
+                <BreadcrumbItems separator="/">
+                  <ActiveLink
+                    href={"/"}
+                    activeClassName="font-semibold text-heading"
+                  >
+                    <a>{t("breadcrumb-home")}</a>
+                  </ActiveLink>
+                  <ActiveLink
+                    href={'/products'}
+                    activeClassName="font-semibold text-heading"
+                  >
+                    <a className="capitalize">{t("products")}</a>
+                  </ActiveLink>
+                </BreadcrumbItems>
+              </div>
+           
+            </StickyBox>
+          </div>
+
           <div className="w-full lg:-ms-9">
             <SearchTopBar total={data?.returnAllProduct?.length} />
             <ProductGrid
